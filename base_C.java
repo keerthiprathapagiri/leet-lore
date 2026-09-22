839bb41a0e119df0812c6bcd2657778039565a5a
Keerthiprathapagiri <prathapagirikeerthi09@gmail.com>
2026-09-22 23:21:54 +0530

    Add base conversion problem

---
+++ base_C.java
@@ -0,0 +1,39 @@
+// Base Conversion
+//
+// Question:
+// Write a Java program to convert a decimal number into a given base,
+// where values 0–9 are represented by digits and values 10 onwards
+// are represented by A, B, C, etc.
+//
+// Input:
+// Number = 718
+// Base = 12
+//
+// Expected Output:
+// 4BA
+
+//converting number to 10-A , 11-B and numbers 0-9
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int n=12;
+      int num=718;
+      int rem =0;
+      StringBuilder res=new StringBuilder();
+      while(num>0){
+        rem=num%n;
+        if(rem<=9){
+          res=res.append(rem);
+        }
+        else{
+          res=res.append((char)('A'+(rem-10)));
+        }
+        num=num/n;
+      }
+      System.out.print(res.reverse());
+      
+    }
+}
