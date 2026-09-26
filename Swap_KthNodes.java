c303ab8e7c745c9c718acde4573ca9ee450209fe
Keerthiprathapagiri <prathapagirikeerthi09@gmail.com>
2026-09-26 15:28:36 +0530

    adding swaping kth nodes in linkde list problem

---
+++ Swap_KthNodes.java
@@ -0,0 +1,25 @@
+// Question: Swap Kth Nodes
+// Operations: Find the kth node from the beginning, find the kth node from the end, and swap their values.
+
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        
+        ListNode f = head;
+        for (int i = 1; i < k; i++)
+            f = f.next;
+
+        ListNode c = f;
+        ListNode l = head;
+
+        while (c.next != null) {
+            c = c.next;
+            l = l.next;
+        }
+
+        int temp = f.val;
+        f.val = l.val;
+        l.val = temp;
+
+        return head;
+    }
+}
