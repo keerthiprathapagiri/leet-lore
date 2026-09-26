85de6c2ea9ac84a9640fb5796634592d549c6cbb
Keerthiprathapagiri <prathapagirikeerthi09@gmail.com>
2026-09-26 12:24:12 +0530

    adding linked list cycle 2 prblm

---
+++ Linked_List_Cycle2.java
@@ -0,0 +1,28 @@
+// Leetcode = 142
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+// Input: head = [1], pos = -1
+// Output: no cycle
+// Explanation: There is no cycle in the linked list.
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                ListNode s=head;
+                while(s!=slow){
+                    s=s.next;
+                    slow=slow.next;
+                    
+                }return s;
+            }
+        }
+        return null;
+        
+    }
+}
+ 
